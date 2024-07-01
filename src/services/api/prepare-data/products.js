48ccc8c7d8b1401812eb7c47655aa7ce5a066b31
Abbosbek <abodeveloper2811@gmail.com>
2024-07-01 16:05:35 +0500

    fix

--- src/services/api/prepare-data/products.js
+++ src/services/api/prepare-data/products.js
@@ -1,10 +1,11 @@
 export function prepareProductForEdit(item) {
   return {
-    name: item.name,
-    category_id: item.category.id,
-    format_id: item.format.id,
-    product_type: item.product_type,
-    price: item.price,
-    bar_code: item.bar_code,
+    name: item?.name,
+    category_id: item?.category?.id,
+    format_id: item?.format?.id,
+    storage_id: item?.storage?.id,
+    product_type: item?.product_type,
+    price: item?.price,
+    bar_code: item?.bar_code,
   };
 }
