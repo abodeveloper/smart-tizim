bf6d4a4751deb001ce948658c8b07ebd64ced427
Abbosbek <abodeveloper2811@gmail.com>
2024-07-01 16:10:48 +0500

    fix

--- src/services/api/prepare-data/products.js
+++ src/services/api/prepare-data/products.js
@@ -1,11 +1,13 @@
+import { get } from "lodash";
+
 export function prepareProductForEdit(item) {
   return {
-    name: item?.name,
-    category_id: item?.category?.id,
-    format_id: item?.format?.id,
-    storage_id: item?.storage?.id,
-    product_type: item?.product_type,
-    price: item?.price,
-    bar_code: item?.bar_code,
+    name: get(item, "name", ""),
+    category_id: get(item, "category.id", ""),
+    format_id: get(item, "format.id", ""),
+    storage_id: get(item, "storage.id", ""),
+    product_type: get(item, "product_type", ""),
+    price: get(item, "price", ""),
+    bar_code: get(item, "bar_code", ""),
   };
 }
