6790b7493e9dc748670fd7097985bb9cc574ef5f
Abbosbek <abodeveloper2811@gmail.com>
2024-07-16 14:44:46 +0500

    fix

--- src/services/api/requests/storage-products.requests.js
+++ src/services/api/requests/storage-products.requests.js
@@ -2,26 +2,26 @@ import request from "../request";
 
 function httpGetStorageProducts(page, page_size, filters) {
   return request.get(
-    `/api/storage-products/?page=${page}&page_size=${page_size}${
+    `/api/storage_products/?page=${page}&page_size=${page_size}${
       filters && `&${filters}`
     }`
   );
 }
 
 function httpGetStorageProductOne(id) {
-  return request.get(`/api/storage-products/${id}/`);
+  return request.get(`/api/storage_products/${id}/`);
 }
 
 function httpPostStorageProduct(data) {
-  return request.post(`/api/storage-products/`, data);
+  return request.post(`/api/storage_products/`, data);
 }
 
 function httpUpdateStorageProduct({ id, data }) {
-  return request.put(`/api/storage-products/${id}/`, data);
+  return request.put(`/api/storage_products/${id}/`, data);
 }
 
 function httpDeleteStorageProduct(id) {
-  return request.delete(`/api/storage-products/${id}/`);
+  return request.delete(`/api/storage_products/${id}/`);
 }
 
 export {
